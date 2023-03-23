6f1a2726e4621aafeef7f975d669d3dd201faa3e
lnwbobby <chinnawatchinnapun@gmail.com>
2023-03-24 00:08:09 +0700

    delete go.mod

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/lnwbobby/testws
-
-go 1.20
--- package_ws/main.go
+++ package_ws/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	// "github.com/lnwbobby/testws/movie"
-	// "github.com/lnwbobby/testws/ticket"
+
+	"github.com/lnwbobby/testws/movie"
+	"github.com/lnwbobby/testws/ticket"
 )
 
 func main() {
 	fmt.Println("asd")
-	// movie.ReviewMovie("God of war ", 9)
-	// miv := movie.FindMovieName("aaa")
-	// fmt.Println(miv)
-	// ticket.BuyTicket("BOBO")
+	movie.ReviewMovie("God of war ", 9)
+	miv := movie.FindMovieName("aaa")
+	fmt.Println(miv)
+	ticket.BuyTicket("BOBO")
 }
